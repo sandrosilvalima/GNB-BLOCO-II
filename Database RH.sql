62c74c0c952e3d32f02f247c96a9ecfedc43bee6
Sandro Silva Lima <88206798+sandrosilvalima@users.noreply.github.com>
2021-08-19 18:07:23 -0300

    Update Database RH.sql

--- Database RH.sql
+++ Database RH.sql
@@ -12,11 +12,11 @@ CREATE TABLE tb_colaboradores(
     
 INSERT INTO tb_colaboradores (nome,sexo,nascimento,estado_civil,salario)
 VALUES
-('SANDRO', 'MASCULINO', 1993-06-06, 'CASADO', 2601.00),
-('FERNANDO', 'MASCULINO', 1993-06-06, 'SOLTEIRO', 1700.75),
-('SONIA', 'FEMININO', 1979-01-07, 'CASADA', 3458.98),
-('ELEN', 'FEMININO', 1998-06-28, 'CASADO', 1100.00),
-('LEONARDO', 'MASCULINO', 1983-11-29, 'CASADO', 4567.35);
+('SANDRO', 'MASCULINO', '1993-06-06', 'CASADO', 2601.00),
+('FERNANDO', 'MASCULINO', '1993-06-06', 'SOLTEIRO', 1700.75),
+('SONIA', 'FEMININO', '1979-01-07', 'CASADA', 3458.98),
+('ELEN', 'FEMININO', '1998-06-28', 'CASADO', 1100.00),
+('LEONARDO', 'MASCULINO', '1983-11-29', 'CASADO', 4567.35);
 
 SELECT nome, salario FROM tb_colaboradores WHERE salario > 2000;
 SELECT nome, salario FROM tb_colaboradores WHERE salario < 2000;
