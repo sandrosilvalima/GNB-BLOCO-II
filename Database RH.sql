771d1f0b86a340ea82d00f990b5e242960358ca5
sandrosilvalima <sandrosilvalima6@gmail.com>
2021-08-19 19:05:53 -0300

    load

--- Database RH.sql
+++ Database RH.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE rh;
 USE rh;
 CREATE TABLE tb_colaboradores(
-	matricula INT NOT NULL AUTO_INCREMENT,
+    matricula INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR (50),
     sexo VARCHAR (20),
     nascimento DATE,
@@ -12,11 +12,11 @@ CREATE TABLE tb_colaboradores(
     
 INSERT INTO tb_colaboradores (nome,sexo,nascimento,estado_civil,salario)
 VALUES
-('SANDRO', 'MASCULINO', 1993-06-06, 'CASADO', 2601.00),
-('FERNANDO', 'MASCULINO', 1993-06-06, 'SOLTEIRO', 1700.75),
-('SONIA', 'FEMININO', 1979-01-07, 'CASADA', 3458.98),
-('ELEN', 'FEMININO', 1998-06-28, 'CASADO', 1100.00),
-('LEONARDO', 'MASCULINO', 1983-11-29, 'CASADO', 4567.35);
+('SANDRO', 'MASCULINO', '1993-06-06', 'CASADO', 2601.00),
+('FERNANDO', 'MASCULINO', '1993-06-06', 'SOLTEIRO', 1700.75),
+('SONIA', 'FEMININO', '1979-01-07', 'CASADA', 3458.98),
+('ELEN', 'FEMININO', '1998-06-28', 'CASADO', 1100.00),
+('LEONARDO', 'MASCULINO', '1983-11-29', 'CASADO', 4567.35);
 
 SELECT nome, salario FROM tb_colaboradores WHERE salario > 2000;
 SELECT nome, salario FROM tb_colaboradores WHERE salario < 2000;
--- registro_escolar.sql
+++
@@ -1,4 +0,0 @@
-
-select * from tb_estudantes;
-insert into tb_estudantes(nome,idade,serie,nota,status) values ("SANDRO SILVA LIMA",17,"3ª ENSINO MÉDIO",9.5,"APROVADO")
-
